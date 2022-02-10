06dee4c3f60f65da5e5c8b0736bff7ff6aa4a0de
Jeonghyeon Yang <yxzzzxh@gmail.com>
2022-02-10 10:07:33 +0900

    Add script argument

--- gen-secret-name.py
+++ gen-secret-name.py
@@ -1,12 +1,15 @@
 # prequsite : pip3 install fnvhash
 # run : python3 gen-secret-name.py
 from fnvhash import fnv1a_32
+import sys
 
 def hash_url(url): 
     h = fnv1a_32(url)
     return h
 
-repoURL = b"https://gitlab.gitlab-system.172.21.5.210.nip.io/root/argocd-installer.git"
+arg = sys.argv
+
+repoURL = bytes(arg[1], 'utf-8')
 hashCode = hash_url(repoURL)
 
 hash_str = str(hashCode)
